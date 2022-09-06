4f3af4fa0086c66150792a8b473fea4a461f9bdb
ITStar000 <ivan.selyutin2020@gmail.com>
2022-09-06 08:42:23 +0300

    authentication

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,23 @@ import CreateLink from './CreateLink';
 import Header from './Header';
 import LinkList from './LinkList';
 import { Route, Routes } from 'react-router-dom';
+import Login from './Login';
 
 const App = () => {
   return (
     <div className="center w85">
-      <Header />
-      <div className="ph3 pv1 background-gray">
-        <Routes>
-          <Route path="/" element={<LinkList/>} />
-          <Route
-            path="/create"
-            element={<CreateLink/>}
-          />
-        </Routes>
-      </div>
+    <Header />
+    <div className="ph3 pv1 background-gray">
+      <Routes>
+        <Route path="/" element={<LinkList/>} />
+        <Route
+          path="/create"
+          element={<CreateLink/>}
+        />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
     </div>
+  </div>
   );
 };
 
